9aab82945e88cf197b8c1cc6f8af46481e25379b
Denis <denismakste@gmail.com>
2023-04-25 00:52:35 +0600

    add: main layout header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Header } from "y/shared-components/Layout";
 
 const HomePage = () => {
   return (
-	<div className='bg-red-500 p-5'>InitPage</div>
-  )
-}
+    <div className="flex h-screen w-full flex-col">
+      <Header></Header>
+      <section className="grid h-full grid-cols-12 place-items-center">
+        <main
+          id="global-main-content"
+          className="col-span-8 h-full w-full border-r border-gray-200"
+        >
+          weq
+        </main>
+        <aside id="global-sidebar" className="col-span-4 w-full">
+          qzxcc
+        </aside>
+      </section>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ src/shared-components/Layout/Header/Header.tsx
@@ -0,0 +1,38 @@
+import type { FC } from "react";
+import type { HeaderProps } from "./Header.types";
+
+import { BsBell } from "react-icons/bs";
+import { FiEdit } from "react-icons/fi";
+import { IoReorderThreeOutline } from "react-icons/io5";
+
+const Header: FC<HeaderProps> = (props) => {
+  return (
+    <header
+      className="flex h-20 w-full flex-row items-center justify-between border-b border-gray-200 bg-white px-4"
+      {...props}
+    >
+      <div className="div">
+        <IoReorderThreeOutline className="cursor-pointer text-4xl text-gray-500"></IoReorderThreeOutline>
+      </div>
+      <div className="text-3xl font-bold text-gray-500">SnapBlog</div>
+      <div className="flex items-center space-x-4">
+        <div>
+          <BsBell className="cursor-pointer text-2xl text-gray-500"></BsBell>
+        </div>
+        <div>
+          <div className="h-5 w-5 rounded-full bg-gray-200"></div>
+        </div>
+        <div>
+          <button className="flex items-center space-x-3 rounded border border-gray-400 px-4 py-2.5 transition hover:border-gray-800">
+            <div>Write</div>
+            <div>
+              <FiEdit />
+            </div>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
